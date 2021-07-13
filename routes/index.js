f6f8540735352659b4f91c4ebadd16aabc34299f
Danny Dykes <danny@dannydykes.com>
2021-07-13 17:09:27 -0500

    updated to used query

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express by Danny!' });
-  var name = req.param('name');
+  var name = req.query('name');
   res.send("Hello " + name + "!");
 });
 
