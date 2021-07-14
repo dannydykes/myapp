4d99386d8e3d4b722c229ad68aae3aeba37a8785
Danny Dykes <danny@dannydykes.com>
2021-07-14 16:53:01 -0500

    different param test

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express by Danny!', name: req.query.name });
-  var name = req.query.name; 
-  res.send("Hello " + name + "! and this is the body " + JSON.stringify(req.body));
-  console.log("name was " + name + JSON.stringify(body));
+  var name = req.params.name; 
+  console.log("Body was " + req.body);
+  res.json(req.body);
+
+  console.log("  name = " + name)
 });
 
 module.exports = router;
