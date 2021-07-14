4280fed359a32ef079e51cf07475cebce4b4df22
Danny Dykes <danny@dannydykes.com>
2021-07-14 16:43:58 -0500

    JSON test

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express by Danny!', name: req.query.name });
   var name = req.query.name; 
   res.send("Hello " + name + "! and this is the body " + JSON.stringify(req.body));
-  console.log("name was " + name);
+  console.log("name was " + name + JSON.stringify(body));
 });
 
 module.exports = router;
