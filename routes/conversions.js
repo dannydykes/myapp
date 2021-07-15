a427ebd017b8daaad47761a61b930840852384fa
Danny Dykes <danny@dannydykes.com>
2021-07-15 11:23:56 -0500

    Added mongodb

--- routes/conversions.js
+++ routes/conversions.js
@@ -1,4 +1,5 @@
-var express = require('express');
+const { MongoClient } = require('mongodb');
+const express = require('express');
 var router = express.Router();
 
 /* GET home page. */
@@ -15,6 +16,36 @@ router.get('/', function(req, res, next) {
 
   console.log(" here is what we heard: value " + cv + " oeg " + oeg + " cc " + cc + " gcl " + gcl + " fwb " + fwb);
   console.log("URL = " + url);
+
+  // Connect to the DB
+  connectDB();
 });
+
+async function connectDB() {
+  // Connect to the database
+  const uri = "mongodb+srv://dannydykes:crQHaubvcNDnjgwF@cluster0.cvvus.mongodb.net/sample_analytics?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  const result = await client.connect(function (err, db) {
+      if (err) {
+          console.log("We have a connection error: " + err);
+          throw err;
+      }
+      var dbo = db.db();
+      console.log("Have have dbo " + dbo.databaseName);
+
+      var collection = dbo.collection('customers');
+      console.log("collection = " + collection.collectionName);
+
+      var result = collection.findOne({}, function (errors, result) {
+          if (errors) {
+              console.log(" Error!! " + errors);
+              throw errors;
+          }
+          console.log(" found one! " + result);
+          db.close();
+      });
+  });
+}
 
 module.exports = router;
