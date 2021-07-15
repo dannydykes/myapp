846cb0452bb326c601fc7ad5e1bbf0144bc15a75
Danny Dykes <danny@dannydykes.com>
2021-07-15 13:16:12 -0500

    inserting into db

--- routes/conversions.js
+++ routes/conversions.js
@@ -6,46 +6,37 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   
   // get the header values
-  var cv = req.get('cv'); 
-  var cc = req.get('cc');
-  var oeg = req.get('oeg');
-  var gcl = req.get('gcl');
-  var fwb = req.get('fwb');
-  var url = req.get('url');
+  var date = new Date();
+  var cv = req.get('cv') !== undefined ? req.get('cv') : 0.00; 
+  var cc = req.get('cc') !== undefined ? req.get('cc') : 'USD';
+  var oeg = req.get('oeg') !== undefined ? req.get('oeg') : 'Not Set';
+  var gcl = req.get('gcl') !== undefined ? req.get('gcl') : 'Not Set';
+  var fwbo = req.get('fwbo') !== undefined ? req.get('fwbo') : 'Not Set';
+  var fwb = req.get('fwb') !== undefined ? req.get('fwb') : 'Not Set';
+  var page_location = req.get('page_location') !== undefined ? req.get('page_location') : 'Not Set';
 
 
-  console.log(" here is what we heard: value " + cv + " oeg " + oeg + " cc " + cc + " gcl " + gcl + " fwb " + fwb);
-  console.log("URL = " + url);
-
-  // Connect to the DB
-  connectDB();
-});
-
-async function connectDB() {
   // Connect to the database
-  const uri = "mongodb+srv://dannydykes:crQHaubvcNDnjgwF@cluster0.cvvus.mongodb.net/sample_analytics?retryWrites=true&w=majority";
+  // Make this URI a ENV variable to hide password (or later even better security)
+  const uri = "mongodb+srv://dannydykes:crQHaubvcNDnjgwF@cluster0.cvvus.mongodb.net/conversions?retryWrites=true&w=majority";
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  const result = await client.connect(function (err, db) {
-      if (err) {
-          console.log("We have a connection error: " + err);
-          throw err;
-      }
-      var dbo = db.db();
-      console.log("Have have dbo " + dbo.databaseName);
-
-      var collection = dbo.collection('customers');
-      console.log("collection = " + collection.collectionName);
-
-      var result = collection.findOne({}, function (errors, result) {
-          if (errors) {
-              console.log(" Error!! " + errors);
-              throw errors;
-          }
-          console.log(" found one! " + result);
-          db.close();
-      });
+  client.connect(function (err, db) {
+    if (err) throw err;
+    
+    var dbo = db.db();
+    var collection = dbo.collection('bd_client');
+    collection.insertOne( { 
+      date: date,
+      cv: cv,
+      cc: cc,
+      oeg: oeg,
+      gcl: gcl,
+      fwbo: fwbo,
+      fwb: fwb,
+      page_location: page_location
+    });
   });
-}
+});
 
 module.exports = router;
